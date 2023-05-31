5ea54a7ec972a6b23b9d61a7d2c50d6ee2864c11
Radhikasuresh <radhikasuresh550@gmail.com>
2023-05-31 19:53:45 +0530

    first commit

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import { studentsRouter } from "./Routers/students.js";
 import dotenv from "dotenv"
 import { userRouter } from "./Routers/users.js";
 import { isAuthenticated } from "./Authentication/auth.js";
+import cors from "cors";
 
 dotenv.config()
 const PORT=process.env.PORT
 const app=express();
 app.use(express.json())
+app.use(cors());
 
 
 app.use("/students",isAuthenticated,studentsRouter)
