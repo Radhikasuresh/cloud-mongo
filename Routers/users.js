55372465b2be97354ed4e5543644caaef56de871
Radhikasuresh <radhikasuresh550@gmail.com>
2023-06-07 17:02:27 +0530

    first commit

--- Routers/users.js
+++ Routers/users.js
@@ -2,47 +2,49 @@ import express from "express"
 import { addUsers, generateJwtToken, getUser } from "../Controllers/users.js";
 import bcrypt from "bcrypt"
 
-const router=express.Router();
-
-
-router.post("/signup",async(req,res)=>{
+const router = express.Router();
+router.post("/signup", async(req, res)=>{
     try {
-        const salt=await bcrypt.genSalt(10);
-        const user=await getUser(req.body.email)
+        const salt = await bcrypt.genSalt(10);
+        const user = await getUser(req.body.email)
         if(!user){
-        const hashedPassword=await bcrypt.hash(req.body.password,salt)
-        const hashedUser=await {...req.body,password:hashedPassword}
-        const result=await addUsers(hashedUser)
-        return res.status(200).json({result:result,data:"Added Successfully"})
-    }
-        res.status(400).json({data:"Given Email already exist"})
+            //logic 
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            const hashedUser= await {...req.body, password: hashedPassword}
+            const result = await addUsers(hashedUser)
+            return res.status(200).json({result, data:"Added Sucessfully"})
+        }
+        res.status(400).json({data:"Given email already exist"})
     } catch (error) {
-        console.log(error)
-        res.status(500).json({data:"Internal Server Error"})
+        console.log(error);
+        res.status(500).json({ data: "Internal server Error" }); 
     }
 })
 
-router.post("/login",async(req,res)=>{
+router.post("/login", async(req, res)=>{
     try {
-        const user=await getUser(req.body.email);
+        // is user available
+        const user = await getUser(req.body.email);
         if(!user){
-            res.status(400).json({data:"Invalid Email"})
+            return res.status(404).json({data:"Invalid Email"})
         }
-        const validPassword=await bcrypt.compare(
+        // is password valid
+        const validPassword = await bcrypt.compare(
             req.body.password,
             user.password
         )
         if(!validPassword){
             return res.status(400).json({data:"Invalid Password"})
         }
-        const token=generateJwtToken(user._id);
+        const token =  generateJwtToken(user._id);
         res.status(200).json({data:{
-            message:"successfully logged in",
-            token:token
+            message :"sucessfully loggedIn",
+            token : token
         }})
     } catch (error) {
-        console.log(error)
-        res.status(500).json({data:"Internal Server Error"})
+        console.log(error);
+        res.status(500).json({ data: "Internal server Error", error: error }); 
     }
 })
-export const userRouter=router;
+
+export const userRouter = router;
